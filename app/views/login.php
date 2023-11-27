af93997c6b28d22a16612671f3599c9a1acf383a
LeynardChequito <144776361+LeynardChequito@users.noreply.github.com>
2023-11-27 12:46:53 +0800

    LogReg Validation

--- app/views/login.php
+++ app/views/login.php
@@ -1,34 +1,76 @@
-<div class="container-fluid service py-6">
-    <div class="container">
-        <div class="text-center wow bounceInUp" data-wow-delay="0.1s">
-            <!-- <small
-                class="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">Our
-                Services</small> -->
-            <a href="/" class="navbar-brand">
-                <!-- <h1 class="text-primary fw-bold mb-0">Cater<span class="text-dark">Serv</span> </h1> -->
-                <img src="<?= base_url() ?>/public/img/logo.png" alt="" class="logo pb-4 mb-4" style="width: 200px;">
-            </a>
-        </div>
-        <div class="row g-4 justify-content-center">
-            <div class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp" data-wow-delay="0.1s">
-                <div class="bg-light rounded ">
-                    <div class="service-content d-flex align-items-center justify-content-center p-4">
-                        <div class="service-content-icon text-center">
-                            <h4 class="mb-3">Login to your account</h4>
-                            <form action="<?= site_url('/login') ?>" method="post" class="row">
-                                <input class="mb-3" type="text" name="email" placeholder="Email" id="email">
-                                <input class="mb-3" type="password" name="password" placeholder="Password"
-                                    id="password">
-                                <button type="submit" class="btn btn-primary px-4 py-2 rounded-pill">Login</button>
-                            </form>
+<!DOCTYPE html>
+<html lang="en">
 
-                            <a href="<?= base_url() ?>/register">
-                                <p class="mb-4 mt-4">wala ka pa acc?</p>
-                            </a>
+<head>
+    <?php
+    include 'head.php';
+    ?>
+</head>
+
+<body>
+
+    <div class="container-fluid service py-6">
+        <div class="container">
+            <div class="text-center wow bounceInUp" data-wow-delay="0.1s">
+            <a href="<?= base_url() ?>/" class="navbar-brand">
+                    <img src="<?= base_url() ?>/public/img/acmcl-logo.png" alt="" class="logo pb-4 mb-4" style="width: 200px;">
+                </a>
+            </div>
+            <div class="row g-4 justify-content-center">
+                <div class="col-lg-4 col-md-6 col-sm-12 wow bounceInUp" data-wow-delay="0.1s">
+                <div class="card bg-success text-white">
+                        <div class="card-body p-4">
+                            <div class="service-content d-flex align-items-center justify-content-center">
+                                <div class="service-content-icon text-center">
+                                    <h4 class="mb-3">Login to your account</h4>
+                                    <form action="<?= site_url('/home') ?>" method="post" class="row g-3">
+                                        <div class="col-12">
+                                            <input type="text" name="email" placeholder="Email" id="email"
+                                                class="form-control" required>
+                                        </div>
+                                        <div class="col-12">
+                                            <input type="password" name="password" placeholder="Password" id="password"
+                                                class="form-control" required>
+                                        </div>
+                                        <div class="col-12">
+                                            <button type="submit"
+                                            class="btn btn-light px-4 py-2 rounded-pill">Login</button>
+                                        </div>
+                                    </form>
+
+                                    <a href="<?= base_url() ?>/register" class="text-white mt-3 d-block">Don't have an
+                                        account? Register here.</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <?php
+    include 'scriptLT.php';
+
+    // Validate form data
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        // Check if fields are not empty
+        if (empty($email) || empty($password)) {
+            echo "<script>alert('Email and Password are required.');</script>";
+        } else {
+            // Your login logic goes here
+            // Example: Check credentials in the database
+
+            // Redirect to home page
+            header("Location: /home");
+            exit();
+        }
+    }
+    ?>
+
+</body>
+
+</html>
--- app/views/register.php
+++ app/views/register.php
@@ -1,35 +1,96 @@
-<div class="container-fluid service py-6">
-    <div class="container">
-        <div class="text-center wow bounceInUp" data-wow-delay="0.1s">
-            <!-- <small
-                class="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">Our
-                Services</small> -->
-            <a href="<?= base_url() ?>/" class="navbar-brand">
-                <!-- <h1 class="text-primary fw-bold mb-0">Cater<span class="text-dark">Serv</span> </h1> -->
-                <img src="<?= base_url() ?>/public/img/logo.png" alt="" class="logo pb-4 mb-4" style="width: 200px;">
-            </a>
-        </div>
-        <div class="row g-4 justify-content-center">
-            <div class="col-lg-3 col-md-6 col-sm-12 wow bounceInUp" data-wow-delay="0.1s">
-                <div class="bg-light rounded ">
-                    <div class="service-content d-flex align-items-center justify-content-center p-4">
-                        <div class="service-content-icon text-center">
-                            <h4 class="mb-3">Create an account</h4>
-                            <form action="<?= site_url('/register') ?>" method="post" class="row">
-                                <input class="mb-3" type="text" name="firstname" placeholder="Firstname" id="firstname">
-                                <input class="mb-3" type="text" name="lastname" placeholder="Lastname" id="lastname">
-                                <input class="mb-3" type="text" name="email" placeholder="Email" id="email">
-                                <input class="mb-3" type="password" name="password" placeholder="Password" id="password">
-                                <button type="submit" class="btn btn-primary px-4 py-2 rounded-pill">Register</button>
-                            </form>
+<!DOCTYPE html>
+<html lang="en">
 
-                            <a href="<?= base_url() ?>/login">
-                                <p class="mb-4 mt-4">Already have an account?</p>
-                            </a>
+<head>
+    <?php
+    // Include head.php
+    include 'head.php';
+    ?>
+</head>
+
+<body>
+
+    <div class="container-fluid service py-6">
+        <div class="container">
+            <div class="text-center wow bounceInUp" data-wow-delay="0.1s">
+                <a href="<?= base_url() ?>/" class="navbar-brand">
+                    <img src="<?= base_url() ?>/public/img/acmcl-logo.png" alt="" class="logo pb-4 mb-4" style="width: 200px;">
+                </a>
+            </div>
+            <div class="row g-4 justify-content-center">
+                <div class="col-lg-6 col-md-8 col-sm-12 wow bounceInUp" data-wow-delay="0.1s">
+                    <div class="card bg-success text-white">
+                        <div class="card-body p-4">
+                            <div class="service-content d-flex align-items-center justify-content-center">
+                                <div class="service-content-icon text-center">
+                                    <h4 class="mb-3">Create an account</h4>
+                                    <!-- PHP validation -->
+                                    <?php
+                                    $firstname = $lastname = $email = $password = "";
+
+                                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                        $firstname = test_input($_POST["firstname"]);
+                                        $lastname = test_input($_POST["lastname"]);
+                                        $email = test_input($_POST["email"]);
+                                        $password = test_input($_POST["password"]);
+
+                                        // Add your validation logic here
+                                        // For example, check if fields are not empty
+                                        if (empty($firstname) || empty($lastname) || empty($email) || empty($password)) {
+                                            echo "<p class='text-danger'>All fields are required.</p>";
+                                        } else {
+                                            // Your registration logic goes here
+                                            // Redirect or display success message
+                                            echo "<p class='text-success'>Registration successful!</p>";
+                                        }
+                                    }
+
+                                    function test_input($data)
+                                    {
+                                        $data = trim($data);
+                                        $data = stripslashes($data);
+                                        $data = htmlspecialchars($data);
+                                        return $data;
+                                    }
+                                    ?>
+                                    <!-- End of PHP validation -->
+
+                                    <!-- Registration form -->
+                                    <form action="<?= site_url('/register') ?>" method="post" class="row g-2">
+                                        <div class="col-12">
+                                            <input class="mb-3 form-control" type="text" name="firstname" placeholder="Firstname" id="firstname" required>
+                                        </div>
+                                        <div class="col-12">
+                                            <input class="mb-3 form-control" type="text" name="lastname" placeholder="Lastname" id="lastname" required>
+                                        </div>
+                                        <div class="col-12">
+                                            <input class="mb-3 form-control" type="email" name="email" placeholder="Email" id="email" required>
+                                        </div>
+                                        <div class="col-12">
+                                            <input class="mb-3 form-control" type="password" name="password" placeholder="Password" id="password" required>
+                                        </div>
+                                        <div class="col-12">
+                                            <button type="submit" class="btn btn-light px-4 py-2 rounded-pill">Register</button>
+                                        </div>
+                                    </form>
+                                    <!-- End of registration form -->
+
+                                    <a href="<?= base_url() ?>login" class="text-white mt-3 d-block">Already have an account? Login here.</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <!-- End of card -->
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <?php
+    // Include scriptLT.php
+    include 'scriptLT.php';
+    ?>
+
+</body>
+
+</html>
