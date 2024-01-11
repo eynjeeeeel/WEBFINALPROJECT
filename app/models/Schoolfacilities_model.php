2b14392868632b923e2266062d5027aa51891233
Lua, Mark <marklua599@gmail.com>
2024-01-11 09:31:35 +0800

    fix

--- app/models/Schoolfacilities_model.php
+++ app/models/Schoolfacilities_model.php
@@ -6,6 +6,15 @@ class Schoolfacilities_model extends Model {
         // Query to select all facilities
         return $this->db->table('SchoolFacilities')->get_all();
     }
-    
+    public function addFacility($name, $description) {
+        // Create an array of data to insert
+        $data = [
+            'Name' => $name,
+            'Description' => $description
+        ];
+
+        // Insert data into the 'SchoolFacilities' table
+        return $this->db->table('SchoolFacilities')->insert($data);
+    }
 }
 ?>
